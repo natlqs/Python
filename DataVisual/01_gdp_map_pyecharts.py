210de5425f22f288b4a13a71706f1cab009bce69
natlqs <natlqs@outlook.com>
2022-06-24 10:45:36 +0800

    a

--- DataVisual/01_gdp_map_pyecharts.py
+++ DataVisual/01_gdp_map_pyecharts.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pyecharts.charts import Map
 from pyecharts import options as opts
-from pyecharts.globals import ThemeType # 引入主题
+from pyecharts.globals import ThemeType  # 引入主题
 
 data = pd.read_excel(r'./DataVisual/gdp.xlsx')  # 读取表格
 province = list(data['province'])
@@ -9,23 +9,21 @@ gdp = list(data['2019gdp'])
 list = [list(z) for z in zip(province, gdp)]
 # print(list)
 
-
 c = (
-    Map(init_opts=opts.InitOpts(width='1900px', height='1000px', theme= ThemeType.LIGHT))        # 初始化地图大小
+    Map(init_opts=opts.InitOpts(width='1900px',
+                                height='1000px',
+                                theme=ThemeType.LIGHT))  # 初始化地图大小
     .set_global_opts(
-        title_opts=opts.TitleOpts(title='2019年各省GDP分布图    单位：亿元'),   # 配置标题
+        title_opts=opts.TitleOpts(title='2019年各省GDP分布图    单位：亿元'),  # 配置标题
         visualmap_opts=opts.VisualMapOpts(
-            min_ = 1000, 
-            max_ = 110000, 
-            range_text = ['GDP总量（亿）颜色区间：', ''], # 分区间
-            is_piecewise = True,    # 定义图例为分段型， 默认为连续的图例
-            pos_top = 'middle',     # 分段位置
-            pos_left = 'left', 
-            orient = 'vertical',
-            split_number = 20 # 分成10个区间
+            min_=1000,
+            max_=110000,
+            range_text=['GDP总量（亿）颜色区间：', ''],  # 分区间
+            is_piecewise=True,  # 定义图例为分段型， 默认为连续的图例
+            pos_top='middle',  # 分段位置
+            pos_left='left',
+            orient='vertical',
+            split_number=20  # 分成10个区间
             # type_='scatter'
-        )
-    )
-    .add('GDP', list, maptype='china')  # 将list传入，地图类型为中国地图
-    .render(r"./DataVisual/map2.html")
-)
+        )).add('GDP', list, maptype='china')  # 将list传入，地图类型为中国地图
+    .render(r"./DataVisual/map2.html"))
--- DataVisual/02_gdp_map_geo.py
+++ DataVisual/02_gdp_map_geo.py
@@ -9,15 +9,14 @@ gdp = list(data['2019gdp'])
 list = [list(z) for z in zip(province, gdp)]
 # print(list)
 d = (
-    Geo()
-    .add_schema(maptype='china')
-    .add(
+    Geo().add_schema(maptype='china').add(
         'geo',
-        list, # 传入数据
-        symbol_size=10, large_threshold=110000, # 设置涟漪大小
-        type_=ChartType.EFFECT_SCATTER, #  地图类型为涟漪图
-    )
-    .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-    .set_global_opts(visualmap_opts=opts.VisualMapOpts(max_=110000), title_opts=opts.TitleOpts(title='2019年各省GDP涟漪图'))
-    .render(r'./DataVisual/Geomap1.html')
-)
+        list,  # 传入数据
+        symbol_size=10,
+        large_threshold=110000,  # 设置涟漪大小
+        type_=ChartType.EFFECT_SCATTER,  # 地图类型为涟漪图
+    ).set_series_opts(label_opts=opts.LabelOpts(
+        is_show=False)).set_global_opts(
+            visualmap_opts=opts.VisualMapOpts(max_=110000),
+            title_opts=opts.TitleOpts(
+                title='2019年各省GDP涟漪图')).render(r'./DataVisual/Geomap1.html'))
