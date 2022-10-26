c04bad13cc218be149f96c69a86345c889af3654
natlqs <natlqs@outlook.com>
2022-10-26 10:23:14 +0800

    a

--- DMC/File_Version1.py
+++ DMC/File_Version1.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # 自动获取不断更新的文件，提取csv文件数据，并写入一个新的文件. 需解决： 存入的数据不再存
-import csv
 import os
 import time
 import threading
@@ -10,16 +9,18 @@ filepath = 'C:\\Natlqs\\Projects\\1# HSCB DMC Result Confirm\\Natlqs\\CSV'
 
 
 def File_Save(filename):
-    with open(filepath + '\\'+ filename) as csvfile:
+    with open(filepath + '\\' + filename) as csvfile:
         reader = csvfile.readlines()
         values = reader[4].split(';')       # 获取第5行数据
         DMC = values[0]
         rotate_speed = values[7]
         ok_result = values[2]
         g_value = values[12]
-        data_to_write =  DMC +';'+ g_value
+        data_to_write = DMC + ';' + g_value
         if (ok_result == 'OK') & (rotate_speed == '180000'):
-            this_week_csv = filepath + '\\' + time.strftime('%Y%m%d %H:%M:%S', time.localtime(get_week_begin(time.time(),0)))[2:8] + '.csv'
+            this_week_csv = filepath + '\\' + \
+                time.strftime('%Y%m%d %H:%M:%S', time.localtime(
+                    get_week_begin(time.time(), 0)))[2:8] + '.csv'
             with open(this_week_csv, 'a') as file_destination:
                 file_destination.write(data_to_write)
                 file_destination.write('\n')
@@ -31,15 +32,16 @@ def Getfiles():     # 获取文件名
     扫描文件夹中的文件，删除8天前的，并返回所有文件的文件名
     '''
     filenames0 = os.listdir(filepath)
-    n_daysago = time.strftime('%Y%m%d %H:%M:%S',time.localtime(get_day_begin(time.time(),-8)))[2:8]
-    today = time.strftime('%Y%m%d %H:%M:%S',time.localtime(get_day_begin(time.time(),0)))[2:8]
+    n_daysago = time.strftime('%Y%m%d %H:%M:%S', time.localtime(
+        get_day_begin(time.time(), -8)))[2:8]
+    today = time.strftime('%Y%m%d %H:%M:%S', time.localtime(
+        get_day_begin(time.time(), 0)))[2:8]
     for i in filenames0:
-        if i[-17:-11] < n_daysago:      # 判断是否为8天前的文件 
-            a = filepath + '\\' + i 
+        if i[-17:-11] < n_daysago:      # 判断是否为8天前的文件
+            a = filepath + '\\' + i
             os.remove(a)  # 如果是七天前的文件，删除
-          #  pass
     global filenames
-    filenames = os.listdir(filepath)  
+    filenames = os.listdir(filepath)
 
 
 def get_day_begin(ts=time.time(), N=0):     # 获取当天起始时间
@@ -50,11 +52,11 @@ def get_day_begin(ts=time.time(), N=0):     # 获取当天起始时间
     return int(time.mktime(time.strptime(time.strftime('%Y-%m-%d', time.localtime(ts)), '%Y-%m-%d'))) + 86400 * N
 
 
-def get_week_begin(ts = time.time(),N = 0):
+def get_week_begin(ts=time.time(), N=0):
     """
     N为0时获取时间戳ts当周的开始时间戳，N为负数时前数N周，N为整数是后数N周，此函数将周一作为周的第一天
     """
-    w = int(time.strftime('%w',time.localtime(ts)))
+    w = int(time.strftime('%w', time.localtime(ts)))
     return get_day_begin(int(ts - w*86400)) + N*604800
 
 
@@ -62,16 +64,19 @@ def File_Write():
     '''
     保存上周六和本周的数据只本周csv文件
     '''
-    this_weekbegin = time.strftime('%Y%m%d %H:%M:%S', time.localtime(get_week_begin(time.time(),0)))[2:8]     # 本周开始日期,周日为第一天       211024
-    next_weekbegin = time.strftime('%Y%m%d %H:%M:%S', time.localtime(get_week_begin(time.time(),1)))[2:8]     # 下周开始日期, 周日为第一天       211031
-    last_saturday = time.strftime('%Y%m%d %H:%M:%S',time.localtime(get_day_begin(get_week_begin(time.time(), 0)-86400,0)))[2:8]       # 上周六        211023 
+    this_weekbegin = time.strftime('%Y%m%d %H:%M:%S', time.localtime(
+        get_week_begin(time.time(), 0)))[2:8]     # 本周开始日期,周日为第一天       211024
+    next_weekbegin = time.strftime('%Y%m%d %H:%M:%S', time.localtime(
+        get_week_begin(time.time(), 1)))[2:8]     # 下周开始日期, 周日为第一天       211031
+    last_saturday = time.strftime('%Y%m%d %H:%M:%S', time.localtime(get_day_begin(
+        get_week_begin(time.time(), 0)-86400, 0)))[2:8]       # 上周六        211023
     for i in filenames:
-        if last_saturday > i[-17:-11]: # < next_weekbegin:      # 判断文件是否为本周或上周六文件文件
+        if last_saturday > i[-17:-11]:  # < next_weekbegin:      # 判断文件是否为本周或上周六文件文件
             File_Save(i)
             pass
-        else: 
+        else:
             pass
-    
+
 
 def Cycle_Refresh():
     print(time.time())
--- crawler/requests & selenium/01_slenium_Edge_webdriver.py
+++ crawler/requests & selenium/01_slenium_Edge_webdriver.py
@@ -1,32 +1,10 @@
-<<<<<<< HEAD
-# 自动打开edge浏览器并打开指定网页
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Edge()
-
-driver.get('https://bing.com')
-
-element = driver.find_element(By.ID, 'sb_form_q')
-element.send_keys('WebDriver')
-element.submit()
-
-time.sleep(5)
-=======
-# 自动打开edge浏览器并打开指定网页
-<<<<<<< HEAD
 from lib2to3.pgen2 import driver
-import time
-from xml.dom.minidom import Element
-=======
-import time
->>>>>>> 5038e2cbc0c277214723426b91c1d04876aea656
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+# 自动打开edge浏览器并打开指定网页
 
 driver = webdriver.Edge()
-
 driver.get('https://bing.com')
 
 element = driver.find_element(By.ID, 'sb_form_q')
@@ -34,5 +12,5 @@ element.send_keys('WebDriver')
 element.submit()
 
 time.sleep(5)
->>>>>>> bf1f8be8cee5b4ba1bbd0820ddf8f312884e089b
+# 自动打开edge浏览器并打开指定网页
 driver.quit
