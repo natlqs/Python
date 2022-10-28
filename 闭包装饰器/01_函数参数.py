e3ff8add2f06abb21bb1af666dfc4fa4410b008c
natlqs <natlqs@outlook.com>
2022-10-28 16:47:33 +0800

    a

--- 闭包装饰器/01_函数参数.py
+++ 闭包装饰器/01_函数参数.py
@@ -4,15 +4,17 @@ def func01():
 # # func01()      # 运行函数
 
 # # 函数名存放函数所在空间的地址
-# print(func01)   #打印函数的地址
+print(func01)   #打印函数的地址
 
 # # 函数名也可以像普通变量一样赋值
-# func02 = func01
-# func02()
+func02 = func01
+func02()
+print(func02)
 
 
-def foo(func):
+def foo(func):   # 将函数名当作参数传入函数内部
     func()
+    print(func)
 
 
 foo(func01)
